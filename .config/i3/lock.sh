af343727b22fc3a914120505d8c89316b09df630
Lievin Christopher <christopher.lievin@etudiant.univ-lille1.fr>
2023-05-27 15:14:06 +0200

    add a new img in i3lock for daytime

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 dunstctl set-paused true
-i3lock -t -i .config/i3/lock.jpg --time-pos="150:h-190" --date-pos="150:h-150" --clock --date-str "$(date "+%A %d %b %Y")" --inside-color=00000000 --ring-color=00000000 --line-uses-inside --keyhl-color=00000000 --bshl-color=00000000 --separator-color=00000000 --insidever-color=00000000 --insidewrong-color=00000000 --ringver-color=00000000 --ringwrong-color=00000000 --ind-pos="x+280:h-70" --radius=20 --ring-width=4 --verif-text="" --wrong-text="" --verif-color="00000000" --time-color="ffffffff" --date-color="ffffffff" --date-font=Hack --time-font=Hack --time-size=40 --date-size=30 --date-align 1 --time-align 1
+lock_img=''
+if ([[ $(date +%T) > "20:00:00" ]] || [[ $(date +%T) < "10:00:00" ]])
+then
+    lock_img='.config/i3/lock_night.jpg'
+else    
+    lock_img='.config/i3/lock_day.jpg'
+fi
+i3lock -t -i "$lock_img" --time-pos="150:h-190" --date-pos="150:h-150" --clock --date-str "$(date "+%A %d %b %Y")" --inside-color=00000000 --ring-color=00000000 --line-uses-inside --keyhl-color=00000000 --bshl-color=00000000 --separator-color=00000000 --insidever-color=00000000 --insidewrong-color=00000000 --ringver-color=00000000 --ringwrong-color=00000000 --ind-pos="x+280:h-70" --radius=20 --ring-width=4 --verif-text="" --wrong-text="" --verif-color="00000000" --time-color="ffffffff" --date-color="ffffffff" --date-font=Hack --time-font=Hack --time-size=40 --date-size=30 --date-align 1 --time-align 1
+
 while pgrep -c i3lock &> /dev/null
 do
     sleep 5
